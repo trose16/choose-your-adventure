ae6f7c6a73a13fa021b1ea53947de4a79ea4de8d
TudorTacal <tudortacal@gmail.com>
2017-01-17 15:28:42 +0000

    User management system is working

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,12 +1,11 @@
 class Users::RegistrationsController < Devise::RegistrationsController
-# before_action :configure_sign_up_params, only: [:create]
-# before_action :configure_account_update_params, only: [:update]
+before_action :configure_sign_up_params, only: [:create]
+before_action :configure_account_update_params, only: [:update]
 
 
   def new
     super
   end
-
 
   def create
     super
