cf51d05e1c1fde92443da37b1178e10726794cf1
TudorTacal <tudortacal@gmail.com>
2017-01-17 14:58:55 +0000

    Username doesn't save in the database yet

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-root 'welcome#index'
+  devise_for :users, controllers: {
+         sessions: 'users/sessions',
+         registrations: 'users/registrations'
+       }
+
+  root 'welcome#index'
 
 end
