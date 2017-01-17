6bfe968fadc71f4f6193028ae98171256c1ef9ca
Asuka Ochi <asukaochi@gmail.com>
2017-01-17 20:28:40 +0000

    change the association between Adventure and Game

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170117184524) do
+ActiveRecord::Schema.define(version: 20170117191143) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
--- spec/models/adventure_spec.rb
+++ spec/models/adventure_spec.rb
@@ -2,6 +2,6 @@ require 'rails_helper'
 
 RSpec.describe Adventure, type: :model do
   it { is_expected.to belong_to :city }
-  it { is_expected.to belong_to :game }
+  it { is_expected.to have_one :game }
   it { is_expected.to have_many :stories }
 end
