bc2961eeb4cdfcc19e4c3701cb1e5fdadf344f88
Asuka Ochi <asukaochi@gmail.com>
2017-01-17 17:44:01 +0000

    Created the City model. Inserted London in the database. Added a checkbox for the cities

--- spec/features/game_feature_spec.rb
+++ spec/features/game_feature_spec.rb
@@ -5,6 +5,8 @@ feature 'Game ' do
     scenario 'it should show the form to choose cities' do
       sign_up('Asuka', 'test@email', 'password')
       expect(page).to have_content('Choose city')
+      select 'London', from: 'cities'
+      expect(page).to have_content 'Jack the Ripper'
     end
   end
 end
