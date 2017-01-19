3ce0c9e52b84dea3ba9fbf287590558c912ce4f7
Asuka Ochi <asukaochi@gmail.com>
2017-01-19 15:02:58 +0000

    Move images to folder

---
+++ app/assets/javascripts/story.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+
+  var button1 = document.getElementById("arrive");
+  button1.addEventListener = function("click", function(){
+    document.getElementById("test").innerHTML = "HI"
+  })
+
+})
