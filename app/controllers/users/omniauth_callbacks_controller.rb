ff37ba8a393aee7400303687674875ffcde9eac9
Simon <simon.conway@condaal.com>
2017-01-20 09:05:43 +0000

    Updated omniauth controller

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,25 +1,25 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
-  devise :omniauthable, omniauth_providers: [:twitter]
-
-
-  def twitter
-  end
-
-
-  def passthru
-    super
-  end
-
-
-  def failure
-    super
-  end
-
-  protected
-
-
-  def after_omniauth_failure_path_for(scope)
-    super(scope)
-  end
+  # devise :omniauthable, omniauth_providers: [:twitter]
+  #
+  #
+  # def twitter
+  # end
+  #
+  #
+  # def passthru
+  #   super
+  # end
+  #
+  #
+  # def failure
+  #   super
+  # end
+  #
+  # protected
+  #
+  #
+  # def after_omniauth_failure_path_for(scope)
+  #   super(scope)
+  # end
 end
